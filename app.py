a0b417d2eeff50b6619f698462d3d1d5478ce881
Ratul Minhaz <minhaz.ratul@gmail.com>
2023-08-10 02:09:35 +0200

    initial implementation of multiple consumers work

--- app.py
+++ app.py
@@ -2,60 +2,37 @@ import csv
 import json
 import logging
 from typing import Any
+
+import orjson
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from kafka import KafkaProducer, KafkaAdminClient
-from kafka.admin import NewTopic
+# from kafka import KafkaProducer, KafkaAdminClient
+# from kafka.admin import NewTopic
 from pydantic import BaseModel, Field, model_validator
 from smart_open import open
-
+from confluent_kafka import Producer
+from confluent_kafka.admin import AdminClient, NewTopic
 from db import Neo4jClient
 
 
-class Admin:
-    def __init__(self, brokers):
-        self.admin = KafkaAdminClient(bootstrap_servers=brokers)
+BOOTSTRAP_SERVERS = "localhost:19092"
+TOPIC_NAME = "companies"
+PARTITION_COUNT = 3
 
-    def topic_exists(self, topic_name):
-        topics_metadata = self.admin.list_topics()
-        return topic_name in topics_metadata
-
-    def create_topic(self, topic_name, num_partitions=1, replication_factor=1):
-        if not self.topic_exists(topic_name):
-            new_topic = NewTopic(
-                name=topic_name,
-                num_partitions=num_partitions,
-                replication_factor=replication_factor,
+admin = AdminClient({"bootstrap.servers": BOOTSTRAP_SERVERS})
+topic = NewTopic(
+                topic=TOPIC_NAME,
+                num_partitions=PARTITION_COUNT,
+                replication_factor=1,
             )
-            self.admin.create_topics([new_topic])
-            print(f"Topic {topic_name} created.")
-        else:
-            print(f"Topic {topic_name} already exists.")
-
-    def close(self):
-        self.admin.close()
 
 
-class Producer:
-    def __init__(self, brokers, topic):
-        self.topic = topic
-        self.producer = KafkaProducer(
-            transactional_id="gleif",
-            enable_idempotence=True,
-            bootstrap_servers=brokers,
-            value_serializer=lambda v: json.dumps(v).encode("utf-8"),
-        )
-
-    def add_company(self, lei: str, company: dict[str, Any]):
-        self.producer.send(
-            self.topic,
-            {"lei": lei, "company": company},
-            lei.encode("utf-8"),
-        )
-        self.producer.flush()
-
-    def close(self):
-        self.producer.close()
+if TOPIC_NAME in admin.list_topics().topics:
+    print(TOPIC_NAME)
+    print(f"Topic {TOPIC_NAME} already exists.")
+else:
+    admin.create_topics([topic])
+    print(f"Topic {TOPIC_NAME} created.")
 
 
 class Schema(BaseModel):
@@ -67,6 +44,12 @@ class Schema(BaseModel):
     country: str | None = Field(None, alias="Entity.LegalAddress.Country")
     postal_code: str | None = Field(None, alias="Entity.LegalAddress.PostalCode")
 
+    class Config:
+        allow_population_by_field_name = True
+        use_enum_values = True
+        json_loads = orjson.loads
+        json_dumps = orjson.dumps
+
     @model_validator(mode='before')
     @classmethod
     def check_empty_string(cls, data: Any) -> Any:
@@ -76,16 +59,23 @@ class Schema(BaseModel):
         return data
 
 
-BOOTSTRAP_SERVERS = ["localhost:19092"]
-TOPIC_NAME = "companies"
-
-admin = Admin(BOOTSTRAP_SERVERS)
-admin.create_topic(TOPIC_NAME, num_partitions=3, replication_factor=1)
-
 app = Flask(__name__)
 cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
 logger = logging.getLogger(__name__)
+
+
+def delivery_callback(err, msg):
+    if err:
+        print("ERROR: Message failed delivery: {}".format(err))
+    else:
+        print(
+            "Produced event to topic {topic}: key = {key:12} value = {value:12}".format(
+                topic=msg.topic(),
+                key=msg.key().decode("utf-8"),
+                value=msg.value().decode("utf-8"),
+            )
+        )
 
 
 @app.route("/load", methods=["GET"])
@@ -93,7 +83,13 @@ def load():
     limit = int(request.args.get("limit", "0"))
 
     try:
-        producer = Producer(BOOTSTRAP_SERVERS, TOPIC_NAME)
+        producer = Producer(
+            {
+                "transactional.id": "gleif",
+                "enable.idempotence": True,
+                "bootstrap.servers": BOOTSTRAP_SERVERS,
+            }
+        )
         with open("./data/gleif.csv") as dataset:
             csv_reader = csv.DictReader(dataset, delimiter=",")
 
@@ -101,13 +97,14 @@ def load():
                 try:
                     app.logger.info(f"Processing record no. {current_num}")
                     obj = Schema(**record)
-                    producer.add_company(obj.lei, obj.model_dump())
+                    producer.produce(TOPIC_NAME, obj.model_dump_json(), callback=delivery_callback)
                 except Exception as e:
                     app.logger.error(f"Error processing record no. {current_num}:", exc_info=True,)
                     continue
                 if (current_num != 0) and (current_num == limit):
                     break
-        producer.close()
+            producer.poll(10000)
+            producer.flush()
         return jsonify({"total_companies": current_num})
     except Exception as err:
         msg = f"Unexpected {err=}, {type(err)=}"
--- consumer.py
+++ consumer.py
@@ -1,43 +1,34 @@
-import json
-import uuid
-
-from kafka import KafkaConsumer
-
 from app import BOOTSTRAP_SERVERS, TOPIC_NAME
 from pprint import pprint
 
 from db import Neo4jClient
 
-
-class Consumer:
-    def __init__(self, brokers, topic, driver, group_id=None):
-        self.driver = driver
-        if group_id is None:
-            # group_id = str(uuid.uuid4())
-            group_id = topic
-        self.consumer = KafkaConsumer(
-            topic,
-            auto_offset_reset="earliest",
-            enable_auto_commit=False,
-            bootstrap_servers=brokers,
-            group_id=group_id,
-            value_deserializer=lambda m: json.loads(m.decode("utf-8")),
-        )
-
-        self.consumer.subscribe([topic])
-
-    def run(self):
-        while True:
-            for message in self.consumer:
-                pprint(message.value)
-                self.driver.create(message.value)
-                self.consumer.commit()
-            self.consumer.commit()
-
-    def close(self):
-        self.consumer.close()
+from confluent_kafka import Consumer
 
 
 neo = Neo4jClient("bolt://localhost:7687", "neo4j", "password")
-consumer = Consumer(BOOTSTRAP_SERVERS, TOPIC_NAME, neo)
-consumer.run()
+
+c = Consumer({
+    'bootstrap.servers': BOOTSTRAP_SERVERS,
+    'group.id': TOPIC_NAME,
+    'auto.offset.reset': 'earliest'
+})
+
+c.subscribe([TOPIC_NAME])
+
+
+while True:
+    msg = c.poll(1.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        print("Consumer error: {}".format(msg.error()))
+        continue
+
+    # print('Received message: {}'.format(msg.value().decode('utf-8')))
+    pprint(msg.value)
+    neo.driver.create(msg.value)
+    c.commit()
+
+c.close()
